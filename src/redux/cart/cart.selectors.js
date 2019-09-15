36e970b5ba74affaaaca6eb1f1a27ad25373bb1d
gedeyandi123 <g3d3.development@gmail.com>
2019-09-16 02:10:19 +0800

    added checkout page

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -17,3 +17,12 @@ export const selectCartItemsCount = createSelector(
   cartItems =>
     cartItems.reduce((accQty, cartItem) => accQty + cartItem.quantity, 0)
 )
+
+export const selectCartTotal = createSelector(
+  [selectCartItems],
+  cartItems =>
+    cartItems.reduce(
+      (accQty, cartItem) => accQty + cartItem.quantity * cartItem.price,
+      0
+    )
+)
